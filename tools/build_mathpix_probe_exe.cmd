5647abb45a7bf5b2b2bb943072809cec520ab1a8
antarxo <antarxo@gmail.com>
2026-08-27 02:15:43 +0300

    Build unified MathpixBridge executable

--- tools/build_mathpix_probe_exe.cmd
+++ tools/build_mathpix_probe_exe.cmd
@@ -6,15 +6,15 @@ echo [1/3] Installing build dependencies...
 py -m pip install --upgrade requests pyinstaller
 if errorlevel 1 goto :fail
 
-echo [2/3] Building MathpixLinesProbe.exe...
+echo [2/3] Building MathpixBridge.exe...
 py -m PyInstaller --noconfirm --clean --onefile --windowed ^
-  --name MathpixLinesProbe ^
+  --name MathpixBridge ^
   --paths tools ^
   tools\mathpix_probe_gui.py
 if errorlevel 1 goto :fail
 
 echo [3/3] Done.
-echo EXE: %CD%\dist\MathpixLinesProbe.exe
+echo EXE: %CD%\dist\MathpixBridge.exe
 start "" "%CD%\dist"
 exit /b 0
 
